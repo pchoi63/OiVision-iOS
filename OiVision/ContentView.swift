b18051e582017c51031c86335d8ff10630e38045
oiCreation <pchoi63@hotmail.com>
2023-09-05 22:42:02 -0400

    Initial Commit

---
+++ OiVision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OiVision
+//
+//  Created by Patrick Choi on 9/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OiVision/OiVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OiVisionApp.swift
+//  OiVision
+//
+//  Created by Patrick Choi on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct OiVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
